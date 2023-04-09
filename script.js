6373906e3936d301b4de6517f961ef14d6f52da0
Ashutosh Singh <ats7800@gmail.com>
2023-04-09 07:15:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let selectedBtn = "Apple";
 let loading = false;
 
+const Chart = document.getElementById("myChart");
+// Chart.style.visibility = "hidden";
+
 const buttons = document.getElementsByClassName("stock-button");
 const submitBtn = document.getElementById("submitBtn");
 
@@ -11,6 +14,31 @@ submitBtn.addEventListener("click", function () {
     stock: selectedBtn,
   };
   postPredict(data);
+
+  // Convert the input date string to a Date object
+  const currDate = new Date(date);
+
+  // Calculate one day later
+  currDate.setDate(currDate.getDate() + 1);
+  const oneDayLater = formatDate(currDate);
+
+  // Reset the date to the original input date
+  currDate.setDate(currDate.getDate() - 1);
+
+  // Calculate five days earlier
+  currDate.setDate(currDate.getDate() - 5);
+  const fiveDaysEarlier = formatDate(currDate);
+
+  console.log("One day later: ", oneDayLater);
+  console.log("Five days earlier: ", fiveDaysEarlier);
+
+  //   fetch(`http://api.marketstack.com/v1/eod
+  //     ? access_key = b4c8f9e44087a3ee81d7cf8e06d08782
+  //     & symbols = AAPL
+  //     & date_from = ${fiveDaysEarlier}
+  //     & date_to = ${oneDayLater}`);
+
+  // Chart.style.visibility = "visible";
 });
 
 // document.addEventListener("click", function () {
@@ -38,7 +66,7 @@ function postPredict(data) {
   if (loading) return;
   loading = true;
   document.getElementById("loadingIcon").style.display = "inline-block";
-  fetch("http://localhost:5000/submit", {
+  fetch("http://localhost:5000/predict", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -48,6 +76,13 @@ function postPredict(data) {
     .then((response) => response.json())
     .then((data) => {
       console.log("Response from server:", data);
+      score = data["stock score"];
+      const emos = document.getElementsByClassName("rnd1");
+      score = parseInt(score);
+      for (i in emos) {
+        if (emos[i].classList) emos[i].classList.remove("showScore");
+      }
+      emos[score].classList.add("showScore");
       document.getElementById("loadingIcon").style.display = "none";
       loading = false;
     })
@@ -56,4 +91,11 @@ function postPredict(data) {
       document.getElementById("loadingIcon").style.display = "none";
       loading = false;
     });
+}
+
+function formatDate(date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
 }
